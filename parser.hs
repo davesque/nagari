f2624bc5edf1724130923798c5de96f03ba2827a
David Sanders <dsanders@fusionbox.com>
2012-08-02 17:20:57 -0600

    Char, fail, return, (?)

--- parser.hs
+++ parser.hs
@@ -32,3 +32,23 @@ becomes xs
     | fst parts == ":=" = Just (fst parts, snd parts)
     | otherwise         = Nothing
     where parts = splitAt 2 xs
+
+-- Char
+char :: Parser Char
+char "" = Nothing
+char (x:xs) = Just (x, xs)
+
+-- Fail
+fail :: Parser a
+fail xs = Nothing
+
+-- Return
+return :: a -> Parser a
+return x xs = Just (x, xs)
+
+-- Predicate decoration operator
+infix 7 ?
+(?) :: Parser a -> (a -> Bool) -> Parser a
+(m ? p) xs = case m xs of
+    Nothing      -> Nothing
+    Just (y, ys) -> if p y then Just(y, ys) else Nothing
