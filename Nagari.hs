3ca2a78db029dbb9c71e470036575fda4eaa735e
David Sanders <dsanders@fusionbox.com>
2013-03-03 13:11:44 -0700

    Convert to cabal package

--- Nagari.hs
+++ Nagari.hs
@@ -1,4 +1,4 @@
-module ParseCore where
+module Nagari where
 
 import Prelude hiding (return, fail, iterate)
 import Data.Char
@@ -200,3 +200,42 @@ number = iterateWhile digit >>- \x -> case x of
 -- | Parses a string equal to `s`.
 accept :: String -> Parser String
 accept s = iterate char (length s) ? (==s)
+
+-----------------
+-- Old parsers --
+-----------------
+
+-- | Parses two chars.
+twochars :: Parser (Char, Char)
+twochars = char # char
+
+-- | Returns the second char of two parsed chars.
+sndChar :: Parser Char
+sndChar = twochars >>> snd
+
+-- | Returns two parsed chars as a string.
+twochars' :: Parser String
+twochars' = char # char >>> (\(x, y) -> [x, y])
+
+-- | Parses a semicolon.
+semicolon :: Parser Char
+semicolon = lit ';'
+
+-- | Parses a single digit char and converts it to an integer value.
+digitVal :: Parser Int
+digitVal = digit >>> digitToInt
+
+-- | Parses a single alphabetical char and converts it to uppercase.
+upcaseLetter :: Parser Char
+upcaseLetter = letter >>> toUpper
+
+-- | Parses two chars of the same value.  The `char` parser parses one char,
+-- which is then passed to `lit` to create another parser which will accept the
+-- same char.
+double :: Parser Char
+double = char >>- lit
+
+-- | Requires a string s to be parsed as a token or an error is thrown.
+require :: String -> Parser String
+require s = token (iterate char (length s) ? (==s))
+          ! err ("Required string '" ++ s ++ "' not found")
