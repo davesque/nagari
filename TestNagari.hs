67f35aa90e06bcab17d7d3f7edd0c8ec5d97677c
David Sanders <dsanders@fusionbox.com>
2013-07-29 23:21:11 -0600

    Update tests

--- TestNagari.hs
+++ TestNagari.hs
@@ -1,31 +1,33 @@
 import Test.QuickCheck
 import qualified Nagari as N
 
--- | The unparsed portion of a result from char should never be longer than the
+-- | Char should always produce a result list of length one for non-empty input
+-- strings.
+prop_charResultSize1 :: String -> Property
+prop_charResultSize1 xs = not (null xs) ==> length result == 1
+    where result = N.runParser N.char xs
+
+-- | Char should always produce a result list of length zero for empty input
+-- strings.
+prop_charResultSize2 :: String -> Property
+prop_charResultSize2 xs = null xs ==> null result
+    where result = N.runParser N.char xs
+
+-- | The unparsed portion of a result from char should always be the tail of
+-- the input string.
+prop_charParsed :: String -> Property
+prop_charParsed xs = not (null xs) ==> head xs == (fst . head $ result)
+    where result = N.runParser N.char xs
+
+-- | The parsed portion of a result from char should always be the head of the
 -- input string.
-prop_char_growth :: String -> Bool
-prop_char_growth xs = all (<= length xs) resultLengths
+prop_charUnparsed :: String -> Property
+prop_charUnparsed xs = not (null xs) ==> tail xs == (snd . head $ result)
     where result = N.runParser N.char xs
-          resultLengths = map (length . snd) result
-
-prop_resultOfCharForNonEmptyHasLengthOne :: String -> Property
-prop_resultOfCharForNonEmptyHasLengthOne xs = not (null xs) ==> length result == 1
-    where result = N.runParser N.char xs
-
-prop_resultOfCharForEmptyIsNull :: String -> Property
-prop_resultOfCharForEmptyIsNull xs = null xs ==> null result
-    where result = N.runParser N.char xs
-
-prop_unparseResultOfCharForNonEmptyIsOneShorter :: String -> Bool
-prop_unparseResultOfCharForNonEmptyIsOneShorter [] = True
-prop_unparseResultOfCharForNonEmptyIsOneShorter xs = origLength - 1 == unparsedLength
-    where result = N.runParser N.char xs
-          origLength = length xs
-          unparsedLength = length . snd . head $ result
 
 main :: IO ()
 main = do
-    quickCheck prop_unparsedResultOfCharIsNeverLonger
-    quickCheck prop_resultOfCharForNonEmptyHasLengthOne
-    quickCheck prop_resultOfCharForEmptyIsNull
-    quickCheck prop_unparseResultOfCharForNonEmptyIsOneShorter
+    quickCheck prop_charResultSize1
+    quickCheck prop_charResultSize2
+    quickCheck prop_charUnparsed
+    quickCheck prop_charParsed
