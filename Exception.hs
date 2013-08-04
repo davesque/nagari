ee12a8c6bf56bcd94d7f62d2143440e0e6f1beba
David Sanders <dsanders@fusionbox.com>
2013-08-04 01:38:38 -0600

    Playing with exceptions

---
+++ Exception.hs
@@ -0,0 +1,51 @@
+data Exception e a =
+    Exception e
+  | Success a deriving (Show, Eq)
+
+
+instance Monad (Exception e) where
+    return = Success
+
+    (Success x)   >>= f = f x
+    (Exception e) >>= _ = Exception e
+
+
+newtype ExceptionT e m a =
+    ExceptionT { runExceptionT :: m (Exception e a) }
+
+
+instance Monad m => Monad (ExceptionT e m) where
+    -- | The `return` function constructs an `Exception` type value with the
+    -- `Success` data constructor, then constucts an `m` type value with the
+    -- function `return` from that monad's instance definition, then constucts
+    -- an `ExceptionT` type value with that, in accordance with the signature
+    -- of `runExceptionT`.
+    return = ExceptionT . return . Success
+
+    -- | The bind function propagates any Exception values without
+    -- transformation and transforms any Success values with the function `f`.
+    (ExceptionT m) >>= f = ExceptionT $
+        m >>= \x -> case x of
+            Success r   -> runExceptionT $ f r
+            Exception l -> return $ Exception l
+
+
+-- | The `throwT` function constucts an `ExceptionT` value using the
+-- `Exception` data constructor to build the `Exception` type value.  It is
+-- effectively the opposite of the `return` function from the instance
+-- definition.
+throwT :: (Monad m) => e -> ExceptionT e m a
+throwT = ExceptionT . return . Exception
+
+
+-- | The `catchT` function takes an `ExceptionT` type value and a function
+-- which will take any exceptional values contained within the `ExceptionT`
+-- type and return another `ExceptionT` which reflects whether or not the
+-- exceptional state has been handled.  Any exceptional states will become
+-- visible if the `Exception` data constructor is pattern matched in the case
+-- statement.
+catchT :: (Monad m) => ExceptionT e m a -> (e -> ExceptionT e m a) -> ExceptionT e m a
+catchT (ExceptionT m) f = ExceptionT $
+    m >>= \x -> case x of
+        Success r   -> return $ Success r
+        Exception l -> runExceptionT $ f l
