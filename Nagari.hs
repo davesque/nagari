d03381539523dcd74e255fd256dbdae41ac73ca7
David Sanders <davesque@gmail.com>
2014-11-07 09:43:22 -0700

    Some new ideas

--- Nagari.hs
+++ Nagari.hs
@@ -2,100 +2,110 @@ module Nagari where
 
 import Control.Monad
 import Data.Char
-import qualified Data.List as L
-import Data.Monoid
-import Prelude hiding (filter, iterate, take, takeWhile, map)
+import Prelude hiding (take)
 import qualified Prelude as P
 
-----------------
--- Data types --
-----------------
 
--- | Parser combinator type.
-newtype Parser a = Parser { runParser :: String -> [(a, String)] }
+-- Position type and helper functions
+------------------------------------------------------------------------------
 
----------------
--- Instances --
----------------
+-- | Stores information about the cursor position during parsing operations.
+data Position = Position { line :: Integer, col :: Integer } deriving (Show)
 
-instance Monoid (Parser a) where
-    -- | The identity function for another parser when combined with `mappend`.
-    mempty = Parser $ const []
+-- | Increments a position to the next line.
+nextLine :: Position -> Position
+nextLine (Position l _c) = Position (l + 1) 0
 
-    -- | Allows forking of parsing logic, concatenating the results of several
-    -- parsers into one parser result.
-    (Parser f) `mappend` (Parser g) = Parser $ \xs ->
-        let fResult = f xs
-            gResult = g xs
-        in fResult ++ gResult
+-- | Increments a position to the next column.
+nextCol :: Position -> Position
+nextCol (Position l c) = Position l (c + 1)
 
-instance MonadPlus Parser where
-    mzero = mempty
-    mplus = mappend
 
-instance Functor Parser where
-    -- | Allows for mapping over parser results with a function `f`.
-    fmap f p = Parser $ \xs ->
-        [(f y, ys) | (y, ys) <- runParser p xs]
+-- Input type
+------------------------------------------------------------------------------
+
+-- | Type of data to be parsed.
+type InputType = String
+
+-- | Input wrapper type which includes meta data about cursor position.
+data Input = Input {
+                mark     :: Position,
+                -- | ^ The cursor position which marks the beginning of the
+                -- current parsing operation.  This position is reset to the
+                -- active cursor position whenever a value is returned in the
+                -- Parser monad.
+                position :: Position,
+                -- | ^ The active cursor position of the parsing operation.
+                -- This position is incremented whenever a character is
+                -- consumed from the input.
+                content  :: InputType
+                -- | ^ The content which is being parsed.
+             } deriving (Show)
+
+
+-- | Result type and helper functions
+------------------------------------------------------------------------------
+
+data Result a = Success { result :: a, unparsed :: Input }
+              | Failure { reason :: String } deriving (Show)
+
+-- | Helper function for creating `Failure` values whose messages are prefixed
+-- with additional information from an `Input` value `i`.
+failure :: Input -> String -> Result a
+failure i msg = Failure $ "At line " ++ l ++ ", column " ++ c ++ ": " ++ msg
+    where l = show . line . mark $ i
+          c = show . col . mark  $ i
+
+
+-- | Parser monad
+------------------------------------------------------------------------------
+
+newtype Parser a = Parser { runParser :: Input -> Result a }
 
 instance Monad Parser where
-    -- | Always succeeds at parsing a value `x`.
-    return x = Parser $ \xs -> [(x, xs)]
+    -- | Always succeeds at parsing the given value.
+    return x = Parser $ \(Input _m p c) -> Success x (Input p p c)
 
-    -- | Allows for combination of parsers.
-    Parser p >>= f = Parser $ \xs ->
-        concat [runParser (f y) ys | (y, ys) <- p xs]
+    -- | Takes a parser `p` and binds its result to a function `f` which uses
+    -- the result value to create and return another parser.  Allows for
+    -- combining of parsers using do notation.
+    p >>= f = Parser $ \xs -> case runParser p xs of
+        Success y ys  -> runParser (f y) ys
+        Failure r     -> Failure r
 
-    -- | Always fails at parsing a value.
-    fail _ = Parser $ const []
+-- | Begins a parsing operation at cursor position 0, 0 with the given parser
+-- `p`.
+run :: Parser a -> InputType -> Result a
+run p i = runParser p (Input empty empty i)
+    where empty = Position 0 0
 
-----------------------
--- Parsers builders --
-----------------------
+-- | Returns a parser that always aborts with the given message.
+abort :: String -> Parser a
+abort msg = Parser $ \i -> failure i msg
 
-err :: String -> Parser a
-err xs = Parser $ \ys -> error $ xs ++ " near '" ++ ys ++ "'\n"
 
--- | Alias for `mplus`.
-and :: Parser a -> Parser a -> Parser a
-and = mplus
+-- Default parsers and parser builders
+--------------------------------------------------------------------------------
 
--- | Builds a parser that first attempts to parse with a parser `p` and falls
--- back to parsing with a parser `q` on failure.
-or :: Parser a -> Parser a -> Parser a
-p `or` q = Parser $ \xs -> case runParser p xs of
-    [] -> runParser q xs
-    r  -> r
+-- | Parses a single character.
+char :: Parser Char
+char = Parser $ \i@(Input m p xs) -> case xs of
+    y:ys -> if   y == '\n'
+            then Success y $ Input m (nextLine p) ys
+            else Success y $ Input m (nextCol p) ys
+    _    -> failure i "More input expected"
 
--- | Builds a parser that first attempts to parse with a parser `p` and falls
--- back to parsing with a parser `q` on failure.  Parser result type uses
--- `Either`.
-or' :: Parser a -> Parser b -> Parser (Either b a)
-p `or'` q = Parser $ \xs ->
-    case runParser p xs of
-    [] -> case runParser q xs of
-          [] -> []
-          r2 -> [(Left y, ys) | (y, ys) <- r2]
-    r1 -> [(Right y, ys) | (y, ys) <- r1]
-
--- | Alias for `fmap`.
-map :: (a -> b) -> Parser a -> Parser b
-map = fmap
-
--- | Succeeds at parsing a single character if the given predicate is true for
--- the parser result.
-takeOneIf :: (Char -> Bool) -> Parser Char
-takeOneIf p = Parser $ \xs -> case xs of
-    []   -> []
-    y:ys -> [(y, ys) | p y]
-
-takeOneIf' :: (Char -> Bool) -> Parser Char
-takeOneIf' p = do
+-- | Parses a single character if the given predicate `p` returns `True` when
+-- applied to the parsed result.
+charIf :: (Char -> Bool) -> String -> Parser Char
+charIf p msg = do
     x <- char
-    if p x then return x else fail ""
+    if p x then return x else abort msg
 
--- | Builds a parser which will apply itself to a string the given number of
--- times.
+-- | Parses a single alphabetical character.
+alpha :: Parser Char
+alpha = charIf isAlpha "expected alphabetical character"
+
 take :: Int -> Parser a -> Parser [a]
 take = replicateM
 
@@ -113,84 +123,3 @@ takeAll p = Parser $ \xs -> case runParser (takeAll' p) xs of
     rs -> let unParsed = snd . last $ rs
               results  = P.map fst rs
           in [(results, unParsed)]
-
--- | Builds a parser that will succeed as long as the predicate `p` is true for
--- characters in the input stream.
-takeWhile :: (Char -> Bool) -> Parser String
-takeWhile p = Parser $ \xs -> case xs of
-    [] -> []
-    _  -> let (xsInit, xsTail) = span p xs
-          in [(xsInit, xsTail) | not . null $ xsInit]
-
--- | Finds the index of the first occurrence of a list `xs` in a list `ys`.
-findIn :: (Eq a) => [a] -> [a] -> Maybe Int
-findIn _ []  = Nothing
-findIn [] _  = Nothing
-findIn xs ys = L.elemIndex True $ L.map (L.isPrefixOf xs) (L.tails ys)
-
--- | Builds a parser which parses a string until an occurrence of string `s` is
--- found.  Fails if nothing is found.
-takeUntil :: String -> Parser String
-takeUntil s = Parser $ \xs -> case findIn s xs of
-    Nothing -> []
-    Just i  -> [splitAt i xs]
-
--- | Builds a parser which performs its action and then consumes any whitespace
--- after the parsed content.
-token :: Parser a -> Parser a
-token p = do
-    x <- p
-    takeWhile isSpace
-    return x
-
--- | Parses a sequence of letters.
-letters :: Parser String
-letters = takeWhile isAlpha
-
--- | Parses a tokenized sequence of letters.
-word :: Parser String
-word = token letters
-
--- | Parses a sequence of digits and returns its integer value.
-number :: Parser Int
-number = map read $ takeWhile isDigit
-
--- | Parses a specific string from the input.
-accept :: String -> Parser String
-accept s = do
-    t <- take (length s) char
-    if s == t then return t else fail ""
-
-------------------
--- Core parsers --
-------------------
-
--- | Parses a single character.
-char :: Parser Char
-char = Parser $ \xs -> case xs of
-    []   -> []
-    y:ys -> [(y, ys)]
-
--- | Parses a single whitespace character.
-space :: Parser Char
-space = takeOneIf isSpace
-
--- | Parses a single alphabetical character.
-alpha :: Parser Char
-alpha = takeOneIf isAlpha
-
--- | Parses a single digit character.
-digit :: Parser Char
-digit = takeOneIf isDigit
-
--- | Parses a single alpha-numerical character.
-alphaNum :: Parser Char
-alphaNum = takeOneIf isAlphaNum
-
--- | Parses one of a given character `x`.
-lit :: Char -> Parser Char
-lit x = takeOneIf (==x)
-
--- | Succeeds at parsing a character which is not the given character `x`.
-unLit :: Char -> Parser Char
-unLit x = takeOneIf (/=x)
